4e531c2608a00b8daef42715f7c0cea79c9e3f23
Tyler Black <blacktylera@gmail.com>
2015-02-23 15:14:22 -0600

    compiling jade

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+
+  grunt.initConfig({
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'app/', src: ['**'], dest: 'public/', filter: 'isFile'}]
+    }
+  },
+  jade: {
+    compile:{
+      files: [{expand:true, cwd: 'app/',src: ['**/*.jade'], dest: 'public/', ext: '.html'}]
+    }
+  }
+});
+
+  grunt.registerTask('default', ['']);
+
+};
