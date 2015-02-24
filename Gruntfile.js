058c6ae3a90aa3f2e4c714bf1816baffd3a6b60e
Tyler Black <blacktylera@gmail.com>
2015-02-24 10:22:37 -0600

    added watch file

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,25 +11,40 @@ module.exports = function(grunt) {
       main: {
         files: [
           {expand: true, cwd: 'app/', src: ['**', '!**/*.jade', '!**/*.{scss,sass'], dest: 'public/', filter: 'isFile'}]
-    }
-  },
-  jade: {
-    compile:{
-      files: [{expand:true, cwd: 'app/',src: ['**/*.jade', '!**/_*.jade'], dest: 'public/', ext: '.html'}]
-    }
-  },
-  sass: {
-    options: {
-        sourceMap: true
+      }
     },
-    dist: {
-        files: {
-            'public/css/main.css': 'app/css/main.scss'
-        }
+    jade: {
+      compile:{
+        files: [{expand:true, cwd: 'app/',src: ['**/*.jade', '!**/_*.jade'], dest: 'public/', ext: '.html'}]
+      }
+    },
+    sass: {
+      options: {
+          sourceMap: true
+      },
+      dist: {
+          files: {
+              'public/css/main.css': 'app/css/main.scss'
+          }
+      }
+  },
+     watch: {
+      other: {
+        files: ['app/**', '!app/**/*.jade', '!app/**/*.{sass,scss}'],
+        tasks: ['copy']
+      },
+      jade: {
+        files: ['app/**/*.jade'],
+        tasks: ['jade']
+      },
+      sass: {
+        files: ['app/**/*.{sass,scss}'],
+        tasks: ['sass']
+      }
     }
-}
 });
 
   grunt.registerTask('default', ['']);
   grunt.registerTask('build', ['clean','copy', 'jade', 'sass']);
+  grunt.registerTask('watch', ['build']);
 };
